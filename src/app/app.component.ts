624c8ba6c810b68adc75e80ceeb2d5eebd5bfc7d
Daniel <daniel.morocho@simetrik.com>
2022-10-31 10:08:14 -0500

    Frontend: Front End for CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+type Blog = {
+  title: string;
+  blog: string;
+  content: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Interface';
+  blog: Blog = {
+    title: '',
+    blog: '',
+    content: '',
+  };
+
+  agregar() {
+    console.log(this.blog);
+  }
 }
